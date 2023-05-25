243369f06b04cdb1a2409e4abb315d9da83b2f8f
Manel <ea3eiz@ea3eiz.com>
2023-05-25 22:52:05 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -11,50 +11,17 @@ MARRON="\33[38;5;138m"
 
   
 
-sudo rm /home/pi/comprueba_internet.txt
-cd /home/pi
-wget www.associacioader.com/comprueba_internet.txt
 
-internet=$(awk "NR==1" /home/pi/comprueba_internet.txt)
-noactualizaciones=$(awk "NR==2" /home/pi/comprueba_internet.txt)
-
-if [ $noactualizaciones = "NOACTUALIZACIONES" ]
-then
-clear
-echo "${AMARILLO}"
-echo""
-echo "***************************************************************************************"
-echo "                             NO HAY NINGUNA ACTUALIZACIÓN                              "
-echo "***************************************************************************************"
-echo "${VERDE}PULSA ENTER PARA SALIR"
-read a                                              
-
-elif [ $internet != "SI" ]
-then
-clea
-echo "${ROJO}"
-echo "***************************************************************************************"
-echo "                    NO TIENES INTERNET Y NO  SE VA A ACTUALIZAR                        "
-echo "***************************************************************************************"
-echo "${VERDE}PULSA ENTER PARA SALIR"
-read a
-sh /home/pi/PI-ADER/ejecutar_ImagenActualizada.sh &
-else
-{
-                        clear
                         echo "${AMARILLO}"                        
                         cd /home/pi/PI-ADER                       
                         git pull
-                        sh /home/pi/PI-ADER/ejecutar_ImagenActualizada.sh &
-                        sleep 3
-                        mv qt_* /home/pi/PI-ADER/qt
-                        chmod 777 -R /home/pi/PI-ADER/qt
-
-}
 
 
-fi
-exit;
+
+
+
+
+
 
 
 
