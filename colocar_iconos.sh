20c93e5d6ec9a49dfb46057d15cbef5b9ca228f6
Manel <ea3eiz@ea3eiz.com>
2023-06-03 12:19:22 +0200

    1

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -22,7 +22,7 @@ echo -n "${AMARILLO}"
 #cp /home/pi/*.desktop /home/pi/Desktop
 #sudo rm /home/pi/*.desktop
 #sudo chmod 777 -R /home/pi/Desktop
-echo "           Esperar a que reorganicen los iconos"
+echo "        Esperar a que se reorganicen los iconos"
 # recupera los iconos que se han borrado y se han convertido en icono de un libro
 # pero no quita los semaforos que se queden encendidos por error al reiniciar
 cp /home/pi/PI-ADER/Desktop/*.* /home/pi/Desktop
