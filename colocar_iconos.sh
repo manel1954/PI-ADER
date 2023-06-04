b5afbf55364580dcba465162f1df01f21f0e5fc4
Manel <ea3eiz@ea3eiz.com>
2023-06-04 12:55:35 +0200

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -36,5 +36,5 @@ cp /home/pi/PI-ADER/Desktop/*.* /home/pi/Desktop
 
 #cp /home/pi/icons.screen0-1904x1064.rc /home/pi/.config/xfce4/desktop
 
-cp /home/pi/icons.screen0-1904x1037.rc /home/pi/.config/xfce4/desktop
+cp /home/pi/icons.screen0-1904x1032.rc /home/pi/.config/xfce4/desktop
 xfdesktop --reload                              
