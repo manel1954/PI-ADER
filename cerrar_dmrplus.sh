683c0794d6e32e3d121aa5b72e94454ba5eba1d4
Manel <ea3eiz@ea3eiz.com>
2023-06-08 19:23:53 +0200

    Update cerrar_dmrplus.sh

--- cerrar_dmrplus.sh
+++ cerrar_dmrplus.sh
@@ -4,7 +4,7 @@ sudo killall -9 MMDVMPLUS
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
 
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sh ejecutar_dmrplus.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
@@ -15,4 +15,4 @@ sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
 
-
+ 
