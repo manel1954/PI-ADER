537aa5400969cafd9b42d19a8dbcb9b8fea50bd2
Manel <ea3eiz@ea3eiz.com>
2023-06-10 19:06:37 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -244,26 +244,10 @@ sleep 2
 sudo rm /home/pi/PI-ADER/Desktop/st-data
 sudo rm /home/pi/Desktop/st-data
 
-panelbm=$(awk "NR==1" /home/pi/paneles_activos.ini)
-if [ "$panelbm" = 'BM=OFF' ];then
+
 cd /home/pi/PI-ADER/qt/
 ./qt_panel_bm
-else
-echo "no hace nada"  
-fi
-sleep 10
-paneldmrplus=$(awk "NR==2" /home/pi/paneles_activos.ini)
-if [ "$paneldmrplus" = 'DMRPLUS=OFF' ];then
-cd /home/pi/PI-ADER/qt/
+
 ./qt_panel_dmrplus
-else
-echo "no hace nada"  
-fi
-sleep 10
-panelradio=$(awk "NR==3" /home/pi/paneles_activos.ini)
-if [ "$panelradio" = 'RADIO=OFF' ];then
-cd /home/pi/PI-ADER/qt/
+
 ./qt_panel_radio
-else
-echo "no hace nada"  
-fi
