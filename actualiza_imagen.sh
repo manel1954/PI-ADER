b018f6db24b5c78f8cdb34458716430356bb57f8
Manel <ea3eiz@ea3eiz.com>
2023-06-10 16:12:32 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,21 +1,9 @@
                 
 #!/bin/bash  
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-
-                                           
-                        clear
-                        echo "${AMARILLO}"                        
+                        
                         cd /home/pi/PI-ADER                       
                         git pull                      
                         sh /home/pi/PI-ADER/ejecutar_ImagenActualizada.sh &
-                        sleep 1
                         mv qt_* /home/pi/PI-ADER/qt
                         chmod 777 -R /home/pi/PI-ADER/qt
 
