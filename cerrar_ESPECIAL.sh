2fb7af3248e558a34567b258585177f70cff7423
Manel <ea3eiz@ea3eiz.com>
2023-06-11 20:59:13 +0200

    Update cerrar_ESPECIAL.sh

--- cerrar_ESPECIAL.sh
+++ cerrar_ESPECIAL.sh
@@ -3,7 +3,7 @@
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
 
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
@@ -13,4 +13,4 @@ cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_ESPECIAL.desktop
 
-sudo killall MMDVMESPECIAL
+sudo killall -9 MMDVMESPECIAL
