63e93946a7d1afe771cc8ccdd9db593a40212b91
Manel <ea3eiz@ea3eiz.com>
2023-06-12 20:32:50 +0200

    1

--- pararserviciosdvs.sh
+++ pararserviciosdvs.sh
@@ -8,7 +8,7 @@ if [ "$estado_dvswitch" = "DVSWITCH=OFF" ];then
 sudo systemctl stop ysfgateway.service
 sudo systemctl stop dmr2ysf.service
 sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
+sudo systemctl stop ircddbgatewayd.service
 sudo systemctl stop md380-emu.service
 sudo systemctl stop mmdvm_bridge.service
 sudo systemctl stop nxdngateway.service
