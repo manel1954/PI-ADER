9dc7576ce074545258d0eebc0c90958389495554
Manel <ea3eiz@ea3eiz.com>
2023-06-13 17:21:51 +0200

    Update ejecutar_svxlink.sh

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Colores 
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
@@ -32,13 +31,13 @@ xterm -geometry 88x17+1274+665 -bg black -fg white -fa ‘verdana’ -fs 9x -T C
 
 
 
-#cd /home/pi/Desktop
-#sudo cp Abrir_svxlink.desktop /home/pi
-#sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
-#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
-#sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
-#sed -i "8c SVXLINK=OFF" /home/pi/status.ini
-#cd /home/pi
-#sudo cp Abrir_svxlink.desktop /home/pi/Desktop
-#
-#sudo rm /home/pi/Abrir_svxlink.desktop
+cd /home/pi/Desktop
+sudo cp Abrir_svxlink.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
+sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
+sed -i "8c SVXLINK=OFF" /home/pi/status.ini
+cd /home/pi
+sudo cp Abrir_svxlink.desktop /home/pi/Desktop
+
+sudo rm /home/pi/Abrir_svxlink.desktop
