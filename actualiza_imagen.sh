f9f79cb8b1cfd039d8565ba9cf2e08f289652b7c
Manel <ea3eiz@ea3eiz.com>
2023-07-03 17:17:25 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -10,8 +10,9 @@ GRIS="\033[0m"
 MARRON="\33[38;5;138m"
                         clear
                         echo "${AMARILLO}"
-                        cd /home/pi/PI-ADER                       
-                        git pull                      
+                        cd /home/pi/PI-ADER   
+                        xterm -geometry 88x16+648+665 -bg black -fg white -fa ‘verdana’ -fs 9x -T CONSOLA_DMR+ -e git pull                  
+                        #git pull                      
                         sh /home/pi/PI-ADER/ejecutar_ImagenActualizada.sh &
                         mv qt_* /home/pi/PI-ADER/qt
                         chmod 777 -R /home/pi/PI-ADER/qt
